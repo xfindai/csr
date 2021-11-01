b5f98a7a140477a75fcca67ee99f3b4a970206a1
Jonathan Plotkin <jonathan@xfind.com>
2021-11-01 11:49:56 +0200

    adding logging to run.sh

--- run.sh
+++ run.sh
@@ -3,5 +3,9 @@ SCRIPT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 cd $SCRIPT_PATH
 git pull
 . venv/bin/activate
-#echo "done $(date)"> test.log
+
+echo "Starting Scheduled pull task at $(date)" > scheduled_tasks.log
+
 python run.py config.yaml
+
+echo "Completed Scheduled pull task at $(date)" > scheduled_tasks.log
