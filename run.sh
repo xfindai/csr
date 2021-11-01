d248450e956f43ae64ae5156b6cfa995e2ed101f
Jonathan Plotkin <jonathan@xfind.com>
2021-11-01 11:45:06 +0200

    updating run.sh

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@ SCRIPT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 cd $SCRIPT_PATH
 git pull
 . venv/bin/activate
-echo "done $(date)"> test.log
-#python run.py config.yaml
+#echo "done $(date)"> test.log
+python run.py config.yaml
